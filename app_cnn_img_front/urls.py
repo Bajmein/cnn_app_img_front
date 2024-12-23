28162639ff4790761c12ef3704f65dc5594eedf5
Bajmein <bajmein13@inacapmail.cl>
2024-12-23 12:07:37 -0300

    ''

--- app_cnn_img_front/urls.py
+++ app_cnn_img_front/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from app_frontal.views import clasificador_imagenes
 
-urlpatterns = [
+urlpatterns: list = [
     path('', clasificador_imagenes, name='clasificador'),
 ]
